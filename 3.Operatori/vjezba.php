b38c8623b33228d1f0038423ad527fb326c776a6
Hrvoje Ivančić <ihrvoje@gmail.com>
2024-05-21 15:30:08 +0000

    Vježba DZ

---
+++ 3.Operatori/vjezba.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = 5;
+$b = 10;
+$c = "vjezba";
+$d = "algebra";
+
+echo $a - $b.'<br>';
+echo $a + $b.'<br>';
+echo $a * $b.'<br>';
+echo $a / $b.'<br>';
+echo $a % $b.'<br>';
+
+$f = $c.' '.$d;
+echo $f.'<br>';
+
+echo $a += $b.'<br>'; 
+
+var_dump ($a == $b); 
+echo '<br>';
+
+echo ++$a.'<br>';
+
+echo --$b.'<br>';
